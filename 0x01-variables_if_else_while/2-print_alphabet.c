5a8132625b6cafd530bac0a3ec7718b4bf46d88d
patrick2030 <neboekene66@gmail.com>
2023-03-20 14:51:10 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include  <stdio.h>
+/**
+ * main - prints the alphabet in lowercase.
+ *  
+ * return: 0 on success
+ */
+int main(void)
+{
+	char c= 'a';
+	while (c <= 'z')
+{
+	putchar(c);
+	c++;
+}
+	putchar('\n');
+					return (0);
+}
+
+
+
+
+
+
+
+
+
+
