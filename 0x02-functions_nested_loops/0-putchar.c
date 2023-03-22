472b22417855011d7c745003fbfb1697635e6ba5
patrick2030 <neboekene66@gmail.com>
2023-03-22 03:35:32 -0700

    ./9-times_table | cat -e

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,24 @@
-#include "0-putchar.c"
+#include "main.h"
 /**
  * main - Entry point
- *
+ * Description: 'print _putchar to stdout'
  * Return: Alway 0 (Sucess)
  */
 
 int main(void)
 {
-	char *h = "_putchar\n";
-
-	while (*h)
-	{
-		_putchar(*h);
-		h++;
-	}
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
+	_putchar('h')
+	_putchar('a')
+	_putchar('r')
+	_putchar('\n')
 	return (0);
+
+}
+
+
 
