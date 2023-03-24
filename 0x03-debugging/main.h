2b83033d41230c68ce212e98a809c5f6f52770c6
patrick2030 <neboekene66@gmail.com>
2023-03-23 19:23:54 -0700

    1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
-intp largest_number(int month, int day; int year);
-int convert_day(int month, int day);
 
-#endif
+#endif /* MAIN_H */
