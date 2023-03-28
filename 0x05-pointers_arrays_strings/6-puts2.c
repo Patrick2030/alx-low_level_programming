2f4f20172e365aa815a418bacd3beb665b44a619
patrick2030 <neboekene66@gmail.com>
2023-03-27 19:39:22 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,16 +3,28 @@
 /**
  * puts2 - Prints out every other char of the string.
  * @str: input string to print.
+ * Return: print
  */
 void puts2(char *str)
 {
-	int i;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	while (str[i] != '\0')
+	while (*y  != '\0')
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+
+		_putchar(str[o]);
+	}
 	}
 	_putchar('\n');
 }
